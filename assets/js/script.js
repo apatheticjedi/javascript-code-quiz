55e7bed791eeb826217d036f6d186a265ccafa93
Duder <thedudeabides1981@hotmail.com>
2022-08-06 13:41:36 -0700

    added timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,24 @@ When all answers are complete or timer reaches zero, hide quiz div and display q
 When initials are submitted, hide quiz-over div and display high-scores div. If "Go back" button is pressed, hide quiz over div and display intro div.
 If Clear high scores button is pressed, all initials are removed.
 If "View High Scores" is clicked, intro div is hidden and high-scores div is displayed. */
+
+var setTimer = document.querySelector('.start-btn').addEventListener('click', function () {
+    var timerEl = document.getElementById('timer');
+    var timeLeft = 60;
+    var timeInterval = setInterval(function () {
+        if (timeLeft > 1) {
+            timerEl.textContent = 'Time Left: ' + timeLeft + ' seconds';
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            timerEl.textContent = 'Time Left: ' + timeLeft + ' second';
+            timeLeft--;
+        } else {
+            timerEl.textContent = "Time's Up!";
+            clearInterval(timeInterval);
+        }
+    }, 1000);
+});
+
+var startQuiz = document.querySelector('.start-btn').addEventListener('click', function() {
+    
+});
